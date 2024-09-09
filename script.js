f37c1f8843063eb6339e355ce3e3efedef38d8ed
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-09 10:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	const freq = {};
+	for(char of str){
+		freq[char] = (freq[char] ||0) + 1;
+	}
+	for(char of str){
+		if(freq[char] === 1){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
